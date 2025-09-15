4c100e5260b7a1ed832c9f236de3450b6857cea1
mkwwd <mkwhwkdud@gmail.com>
2025-09-15 18:38:13 +0900

    Feat: 서비스점검페이지추가

--- FrontEnd/DDIB/src/middleware.ts
+++ FrontEnd/DDIB/src/middleware.ts
@@ -1,13 +1,25 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const MAINTENANCE_MODE = process.env.NEXT_PUBLIC_MAINTENANCE === "true";
+
 export async function middleware(request: NextRequest) {
-  console.log("ss");
-  if (!request.cookies.has("state")) {
+  const { pathname } = request.nextUrl;
+
+  if (
+    MAINTENANCE_MODE &&
+    !request.nextUrl.pathname.startsWith("/maintenance")
+  ) {
+    return NextResponse.redirect(new URL("/maintenance", request.url));
+  }
+
+  if (pathname.startsWith("/order") && !request.cookies.has("state")) {
     return NextResponse.redirect(new URL("/products/wait", request.url));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/order"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
