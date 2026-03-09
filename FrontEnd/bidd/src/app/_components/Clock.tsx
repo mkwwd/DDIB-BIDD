003e70b9e94f793c562e734d3b7de0b76102a781
mkwwd <mkwhwkdud@gmail.com>
2026-03-10 05:07:01 +0900

    Feat: 메인화면 타이머 모델 생성

--- FrontEnd/bidd/src/app/_components/Clock.tsx
+++ FrontEnd/bidd/src/app/_components/Clock.tsx
@@ -1,17 +1,19 @@
 import styles from "./clock.module.scss"
-{}
+
 interface ClockProps{
     hourDeg : number;
     minDeg: number;
+    secDeg: number;
 }
 
-const Clock = ({hourDeg, minDeg} : ClockProps) => {
+const Clock = ({hourDeg, minDeg, secDeg} : ClockProps) => {
 
     return(
         <div className={styles.clockBg}>
             <div className={styles.track}></div>
             <div className={styles.hourHand} style={{ transform: `rotate(${hourDeg}deg) translateX(-50%)` }}></div>
             <div className={styles.minHand} style={{ transform: `rotate(${minDeg}deg) translateX(-50%)` }}></div>
+            <div className={styles.secHand} style={{ transform: `rotate(${secDeg}deg) translateX(-50%)` }}></div>
         </div>
     )
     
--- FrontEnd/bidd/src/app/_components/MainSlider.tsx
+++ FrontEnd/bidd/src/app/_components/MainSlider.tsx
@@ -5,46 +5,92 @@ import React, { useState, useRef, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Clock from "./Clock";
+import TimeUnit from "./TimeUnit";
 
 interface TimeDeg{
   hourDeg : number;
   minDeg : number;
+  secDeg : number;
+  hourLeft : string;
+  minLeft : string;
+  secLeft : string;
+}
+
+const getTimeData = () => {
+  const now = new Date();
+  
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+  const ms = now.getMilliseconds();
+  
+  const hDeg = ((h%12) + m / 60) * 30;
+  const mDeg = (m + s/60) * 6;
+  const sDeg = (s + ms /1000) * 6;
+
+  const target = new Date();
+
+  if(h < 12){
+    target.setHours(12, 0, 0 ,0);
+  }else{
+    target.setHours(24, 0, 0, 0);
+  }
+
+  const diff = target.getTime() - now.getTime();
+  
+  const rHour = Math.floor((diff / (1000*60*60)) % 24).toString().padStart(2, '0');
+  const rMin = Math.floor((diff / (1000*60)) % 60).toString().padStart(2, '0');
+  const rSec = Math.floor((diff / 1000) % 60).toString().padStart(2, '0');
+  
+  return{
+    hourDeg : hDeg,
+    minDeg : mDeg,
+    secDeg : sDeg,
+    hourLeft : rHour,
+    minLeft : rMin,
+    secLeft : rSec,
+  };
 }
 
 const MainSlider: React.FC = () => {
 
-  const [timeDeg, setTimeDeg] = useState<TimeDeg>({hourDeg : 0, minDeg : 0});
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [timeData, setTimeData] = useState<TimeDeg>(getTimeData);
 
   useEffect(() => {
 
-    const timeInterval = setInterval(() => {
+    let frameId : number;
 
-      const now = new Date();
-      const hours = now.getHours();
-      const minutes = now.getMinutes();
-      const seconds = now.getSeconds();
-  
-      setTimeDeg({
-        hourDeg : (hours * 30) + (minutes * 0.5),
-        minDeg : (minutes * 6) + (seconds * 0.1),
-      });
+    const updateClock = () => {
+      const newData = getTimeData();
+      setTimeData(newData);
+      frameId = requestAnimationFrame(updateClock);
+    };
 
-      console.log(timeDeg)
-    }, 1000);
+    updateClock();
 
     return () => {
-      clearInterval(timeInterval);
+      cancelAnimationFrame(frameId);
     }
-  })
+  },[])
+
+  const getSectionClass = (idx:number) : string => {
+    return `${styles.sectionWrapper} ${currentIndex === idx ? styles.activeSection : ''}`;
+  }
 
 
   return (
     <div className={styles.container}>
-      <section className={styles.sectionOne}>
-        <Clock hourDeg={timeDeg.hourDeg} minDeg={timeDeg.minDeg}></Clock>
+      <section className={getSectionClass(0)}>
+        <Clock hourDeg={timeData.hourDeg} minDeg={timeData.minDeg} secDeg={timeData.secDeg}></Clock>
         <div className={styles.timer}>
-          <div></div>
-          <div></div>
+          <div className={styles.timerDisplay}>
+            <TimeUnit value={timeData.hourLeft} label="Hours"></TimeUnit>
+            <div className={styles.separator}>:</div>
+            <TimeUnit value={timeData.minLeft} label="Mins"></TimeUnit>
+            <div className={styles.separator}>:</div>
+            <TimeUnit value={timeData.secLeft} label="Secs"></TimeUnit>
+          </div>
         </div>
       </section>
       <section className={styles.sectionTwo}></section>
